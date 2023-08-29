163cf06cfdc87912933a313bebc5fca130b7e3c5
Ahmad Barqawi <Barqawi.88@outlook.com>
2023-08-29 21:42:32 +0100

    add openai api

--- intelliserver/config.js
+++ intelliserver/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     API_KEY: process.env.API_KEY || 'root',
     ADMIN_KEY: process.env.ADMIN_KEY || 'root',
+    USE_DEFAULT_KEYS: true,
     /* make sure to create .env file before making it true */
-    USE_API_AUTH: (process.env.IS_API_AUTH === 'true') || false
+    USE_API_AUTH: true
 };
--- intelliserver/remotes/openai/index.js
+++ intelliserver/remotes/openai/index.js
@@ -1,9 +1,58 @@
 var express = require('express');
 var router = express.Router();
+const { OpenAIWrapper } = require('intellinode');
+
+const { USE_DEFAULT_KEYS } = require('../../config');
+
+// Create a global instance of OpenAI
 
 /* GET api. */
 router.get('/', function(req, res, next) {
-  res.json({ status: "OK", message: "OpenAI Micro Service Underdevelopment!" });
+  res.json({ status: "OK", message: "OpenAI Micro Service is active!" });
 });
+
+function getAPIWrapper(req) {
+
+    if (USE_DEFAULT_KEYS) {
+        return new OpenAIWrapper(process.env.OPENAI_API_KEY);
+    } else {
+        return new OpenAIWrapper(req.body.model_key);
+    }
+}
+
+router.get('/generate_text', async (req, res, next) => {
+    try {
+        const openai = getAPIWrapper(req);
+
+        console.log('generate text parameters: ', req.body.params)
+
+        const result = await openai.generateText(req.body.params);
+        res.json({ status: "OK", data: result });
+    } catch (error) {
+        res.json({ status: "ERROR", message: error.message });
+    }
+});
+
+router.get('/generate_embeddings', async (req, res, next) => {
+    try {
+        const openai = getAPIWrapper(req);
+        const result = await openai.getEmbeddings(req.body.params);
+        res.json({ status: "OK", data: result });
+    } catch (error) {
+        res.json({ status: "ERROR", message: error.message });
+    }
+});
+
+router.get('/generate_images', async (req, res, next) => {
+    try {
+        const openai = getAPIWrapper(req);
+        const result = await openai.generateImages(req.body.params);
+        res.json({ status: "OK", data: result });
+    } catch (error) {
+        res.json({ status: "ERROR", message: error.message });
+    }
+});
+
+
 
 module.exports = router;
