136c2f7adc2a380fa0fe40ec8ee20c9b44d8e6ad
Ahmad Barqawi <Barqawi.88@outlook.com>
2023-09-02 10:00:59 +0100

    add comments to describe the config

--- intelliserver/config.js
+++ intelliserver/config.js
@@ -1,7 +1,10 @@
 module.exports = {
+    /* the API access key when USE_API_AUTH is set to true */
     API_KEY: process.env.API_KEY || 'root',
+    /* the password to access the admin page. The priority is given to the value in the .env */
     ADMIN_KEY: process.env.ADMIN_KEY || 'root',
+    /* when set to true, the server will load the .env file keys. if false, the user should send the keys  */
     USE_DEFAULT_KEYS: true,
-    /* make sure to create .env file before making it true */
+    /* make sure to create the .env file before setting this to true */
     USE_API_AUTH: true
 };
