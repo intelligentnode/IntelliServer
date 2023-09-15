f4ec3628cb0102bf39c2a4c17678a1b48249fb12
Ahmad Barqawi <Barqawi.88@outlook.com>
2023-09-15 19:33:58 +0100

    add the docs for replicate

--- intelliserver/api/models/remote/replicate/index.js
+++ intelliserver/api/models/remote/replicate/index.js
@@ -14,6 +14,61 @@ function getAPIWrapper(req) {
   }
 }
 
+/**
+ * @swagger
+ * /replicate/predict:
+ *   post:
+ *     tags:
+ *       - Models
+ *     summary: Predicts the output based on the input data using Replicate's AI model.
+ *     security:
+ *       - ApiKeyAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - api_key
+ *               - params
+ *             properties:
+ *               api_key:
+ *                 type: string
+ *                 description: The api key for Replicate.
+ *               params:
+ *                 type: object
+ *                 properties:
+ *                   model_tag:
+ *                     type: string
+ *                     description: The model tag to be used for prediction.
+ *                   input_data:
+ *                     type: object
+ *                     properties:
+ *                       version:
+ *                         type: string
+ *                         description: Version string.
+ *                       input:
+ *                         type: object
+ *                         properties:
+ *                           prompt:
+ *                             type: string
+ *                             description: The prompt for the AI model.
+ *                           max_new_tokens:
+ *                             type: integer
+ *                             description: The maximum number of new tokens to be generated.
+ *                           temperature:
+ *                             type: float
+ *                             description: The randomness factor.
+ *                           debug:
+ *                             type: boolean
+ *                             description: Whether to perform debug or not.
+ *     responses:
+ *       200:
+ *         description: The prediction result from Replicate.
+ *       400:
+ *         description: There was a problem with the request.
+ */
 router.post('/predict', async (req, res, next) => {
   try {
     const replicate = getAPIWrapper(req);
@@ -24,6 +79,40 @@ router.post('/predict', async (req, res, next) => {
   }
 });
 
+/**
+ * @swagger
+ * /replicate/status:
+ *   post:
+ *     tags:
+ *       - Models
+ *     summary: Checks the status of a prediction using Replicate.
+ *     security:
+ *       - ApiKeyAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - api_key
+ *               - params
+ *             properties:
+ *               api_key:
+ *                 type: string
+ *                 description: The api key for Replicate.
+ *               params:
+ *                 type: object
+ *                 properties:
+ *                   prediction_id:
+ *                     type: string
+ *                     description: The ID of the prediction.
+ *     responses:
+ *       200:
+ *         description: The status of the prediction.
+ *       400:
+ *         description: There was a problem with the request.
+ */
 router.post('/status', async (req, res, next) => {
     try {
         const replicate = getAPIWrapper(req);
