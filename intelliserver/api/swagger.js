8f6bb862f11f55dba416b31101a97ab6932bbe97
Ahmad Barqawi <Barqawi.88@outlook.com>
2023-09-15 23:47:08 +0100

    update the source

--- intelliserver/api/swagger.js
+++ intelliserver/api/swagger.js
@@ -12,11 +12,6 @@ const options = {
         },
       }
     },
-    servers: [
-      {
-        url: '/api',
-      },
-    ],
     info: {
       title: 'IntelliServer APIs', // Title (required)
       version: '0.0.1',
