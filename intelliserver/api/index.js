0e661a60a0b5fd230589a51db46110bf8eee5a92
Ahmad Barqawi <Barqawi.88@outlook.com>
2023-09-15 23:17:06 +0100

    Update index.js

--- intelliserver/api/index.js
+++ intelliserver/api/index.js
@@ -10,6 +10,7 @@ const config = require('../config');
 // swagger
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger');
+const swaggerUiAssetPath = require("swagger-ui-dist").getAbsoluteFSPath()
 
 //create the app
 var app = express();
@@ -20,7 +21,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('public'))
+app.use(express.static(swaggerUiAssetPath))
+app.use('/public', express.static('public'));
 
 /* ### the api routs ### */
 // # middlwares
