9daa03a465473cb2243cf229641e6a631b9e42d2
cybercoder <barqawi.88@outlook.com>
2024-01-03 21:26:13 +0300

    Update swagger.js
    add more context

--- intelliserver/swagger.js
+++ intelliserver/swagger.js
@@ -14,8 +14,8 @@ const options = {
     },
     info: {
       title: 'IntelliServer APIs',
-      version: '0.0.1',
-      description: 'AI model as scalable microservices, enabling evaluation of LLMs and offering end-to-end AI functions such as chatbot, semantic search, image generation and beyond.'
+      version: '0.1.0',
+      description: 'IntelliServer is a microservice framework providing a scalable interface to various AI tasks like chatting, semantic search, image generation, OCR, and beyond. It enables easy integration of various Large Models and other AI functionalities. Users can deploy IntelliServer locally or in the cloud and utilize its API to enhance their business applications with cutting-edge AI capabilities. You can use Intellinode cloud to upload your docs and augment the AI models with your data using one key concept.'
     },
   },
   apis: ['./api/**/*.js'],
